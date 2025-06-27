bb0fcd9deedfa57b83dbd97c54b551cfb923b220
Adrien Chouteau <achouteau-ext@ponant.com>
2025-06-27 12:06:24 +0200

    feat(supabase): export simple supabase client

---
+++ src/supabase-client.ts
@@ -0,0 +1,24 @@
+import { AppState } from 'react-native';
+import 'react-native-url-polyfill/auto';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
+
+export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
+
+AppState.addEventListener('change', async (state) => {
+  if (state === 'active') {
+    await supabaseClient.auth.startAutoRefresh();
+  } else {
+    await supabaseClient.auth.stopAutoRefresh();
+  }
+});
